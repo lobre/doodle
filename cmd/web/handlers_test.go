da492d12ca013791965be694719dff2ad435ba2d
Loric Brevet <loric.brevet@amersports.com>
2020-11-15 17:37:07 +0100

    Add some e2e tests

---
+++ cmd/web/handlers_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestPing(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+
+	code, _, body := ts.get(t, "/ping")
+
+	if code != http.StatusOK {
+		t.Errorf("want %d; got %d", http.StatusOK, code)
+	}
+
+	if string(body) != "OK" {
+		t.Errorf("want body to equal %q", "OK")
+	}
+}
---
+++ cmd/web/testutils_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/cookiejar"
+	"net/http/httptest"
+	"testing"
+)
+
+func newTestApplication(t *testing.T) *application {
+	return &application{
+		errorLog: log.New(ioutil.Discard, "", 0),
+		infoLog:  log.New(ioutil.Discard, "", 0),
+	}
+}
+
+type testServer struct {
+	*httptest.Server
+}
+
+func newTestServer(t *testing.T, h http.Handler) *testServer {
+	ts := httptest.NewServer(h)
+
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ts.Client().Jar = jar
+
+	// don't follow redirection
+	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
+	return &testServer{ts}
+}
+
+func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, []byte) {
+	rs, err := ts.Client().Get(ts.URL + urlPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rs.Body.Close()
+
+	return rs.StatusCode, rs.Header, body
+}
